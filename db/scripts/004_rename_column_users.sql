b48b9dbb131ddfd6184d8d6229a7639cc9ab559a
Dimarik <krasnodar93@mail.ru>
2022-12-16 13:21:50 +0300

    1. Конфигурация Hibernate [#6872 #371323]16-12-2

---
+++ db/scripts/004_rename_column_users.sql
@@ -0,0 +1,1 @@
+alter table users rename name to login;
---
+++ src/main/java/ru/job4j/cars/model/repository/UserRepository.java
@@ -0,0 +1,134 @@
+package ru.job4j.cars.model.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+import ru.job4j.cars.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserRepository {
+    private final SessionFactory sf;
+    private final Session session;
+
+
+    public UserRepository() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        session = sf.openSession();
+    }
+
+    public UserRepository(SessionFactory sf) {
+        this.sf = sf;
+        session = sf.openSession();
+    }
+
+    /**
+     * В сессию вынесли в поля объекта для оптимизации
+     * закрываем сессию перед удалением объекта
+     */
+    @Override
+    public void finalize() {
+        session.close();
+    }
+
+    /**
+     * Сохранить в базе.
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public User create(User user) {
+        User result = new User((int)session.save(user), user.getLogin(), user.getPassword());
+        return result;
+    }
+
+    /**
+     * Обновить в базе пользователя.
+     * @param user пользователь.
+     */
+    public void update(User user) {
+        try {
+            session.beginTransaction();
+            session.createQuery("Update Item set login = :fLogin, password = :fPassword WHERE id = :fId")
+                    .setParameter("fLogin", user.getLogin())
+                    .setParameter("password", user.getPassword())
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    /**
+     * Удалить пользователя по id.
+     * @param userId ID
+     */
+    public void delete(int userId) {
+        try {
+            session.beginTransaction();
+            session.createQuery("DELETE Item Where id = :fId")
+                    .setParameter("fId", userId)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    /**
+     * Список пользователь отсортированных по id.
+     * @return список пользователей.
+     */
+    public List<User> findAllOrderById() {
+        Query query = session.createQuery("from Item order by id");
+        return query.list();
+    }
+
+    /**
+     * Найти пользователя по ID
+     * @return пользователь.
+     */
+    public Optional<User> findById(int id) {
+        Query query = session.createQuery("from item where id = :fId")
+                .setParameter("fId", id);
+        List<User> listuser = query.list();
+        if (listuser.size() > 0) {
+            return Optional.of(listuser.get(0));
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Список пользователей по login LIKE %key%
+     * @param key key
+     * @return список пользователей.
+     */
+    public List<User> findByLikeLogin(String key) {
+        Query query = session.createQuery("from Item where name Like %:fKey%")
+                .setParameter("fKey", key);
+        return query.list();
+    }
+
+    /**
+     * Найти пользователя по login.
+     * @param login login.
+     * @return Optional or user.
+     */
+    public Optional<User> findByLogin(String login) {
+        Query query = session.createQuery("from Item where name = :login")
+                .setParameter("login", login);
+        List<User> listUser = query.list();
+        if (listUser.size() > 0) {
+            return Optional.of(listUser.get(0));
+        }
+        return Optional.empty();
+    }
+}
