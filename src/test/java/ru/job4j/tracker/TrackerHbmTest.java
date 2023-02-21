f81829555bc69e81a16a8e7853e26efa82ec868f
Dimarik <krasnodar93@mail.ru>
2023-02-21 20:12:24 +0300

    1. Интеграционное тестирование с Hibernate [#6875]

---
+++ src/test/java/ru/job4j/tracker/TrackerHbmTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.model.Item;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+public class TrackerHbmTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            Item result = tracker.findById(item.getId());
+            assertThat(result.getName()).isEqualTo("test1");
+        }
+    }
+
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItemFindBy() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            Item result = tracker.findByName(item.getName()).get(0);
+            assertThat(result.getName()).isEqualTo("test1");
+        }
+    }
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItemDelete() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            tracker.delete(item.getId());
+            Item result = tracker.findById(item.getId());
+            assertThat(result).isNull();
+        }
+    }
+}
